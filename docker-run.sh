fb606384217fa4fbd657cc969c1a6e005f20027f
JamesHeppell <jheppell100@hotmail.com>
2024-06-28 08:53:29 +0100

    Update docker-run.sh for playstrategy

--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 docker run \
-    --mount type=bind,source=$HOME/dev/lichess,target=/home/lichess/projects \
+    --mount type=bind,source=$HOME/dev/playstrategy,target=/home/playstrategy/projects \
     --publish 9663:9663 \
     --publish 9664:9664 \
-    --publish 8212:8212 \
-    --name lichess \
+    --publish 8210:8210 \
+    --name playstrategy2 \
     --interactive \
     --tty \
-    lichess
+    --memory=6g \
+    --memory-swap=6g \
+    -e JAVA_OPTS='-Xmx3g -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false' \
+    -p 9010:9010 \
+    playstrategy2
