ab6f953c53309c3946b33f3297a9ab6ad47a050f
JamesHeppell <jheppell100@hotmail.com>
2024-06-28 08:55:26 +0100

    Update docker-run.bat for playstrategy

--- docker-run.bat
+++ docker-run.bat
@@ -1,9 +1,11 @@
 docker run ^
-    --mount type=bind,source=C:\dev\lichess,target=/home/lichess/projects ^
+    --mount type=bind,source=D:\dev\playstrategy,target=/home/playstrategy/projects ^
     --publish 9663:9663 ^
     --publish 9664:9664 ^
     --publish 8212:8212 ^
     --interactive ^
     --tty ^
-    --name lichess ^
-    lichess
+    --name playstrategy ^
+    --memory=4g ^
+    --memory-swap=4g ^
+    playstrategy
